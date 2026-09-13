288629dd997eb6338e6863a3cac46f94fb33ca25
AdrianLee <HeavenAndHell060055@gmail.com>
2026-09-13 22:59:37 +0800

    Revert "Task-WUI-v2-m3c: foldable component sections for Properties via the tree"
    This reverts commit 1d2badb415d47e88731ebebfadcea041b9cb5195.

--- Editor/src/WUI/Panels/PropertiesPanel.h
+++ Editor/src/WUI/Panels/PropertiesPanel.h
@@ -2,9 +2,6 @@
 
 #include "EditorPanel.h"
 #include "World/Scene/Components.h"
-#include "World/WUI/WuiWidget.h"
-
-#include <memory>
 
 namespace World
 {
@@ -18,19 +15,10 @@ namespace World
 		void OnRender(Wui::WuiContext& ctx, const Wui::WuiRect& rect, PanelHost& host) override;
 
 	private:
-		struct SectionEntry
-		{
-			std::string Name;
-			std::shared_ptr<Wui::WuiSection> Section;
-		};
-
 		float DrawSchemaFields(Wui::WuiContext& ctx, Wui::WuiId base, const Wui::WuiRect& rect, void* instance,
 			const std::string& typeName, const Schema::TypeSchema& schema);
 		float DrawComponentInspector(Wui::WuiContext& ctx, const Wui::WuiRect& rect, Entity entity, const Schema::TypeSchema& schema);
 
 		PanelHost& m_Host;
-		std::shared_ptr<Wui::WuiBox> m_Root;
-		std::vector<SectionEntry> m_Sections;
-		std::vector<std::string> m_LastSchemaNames;
 	};
 }
